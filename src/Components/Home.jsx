cea907f6994ce9663d9b78d35f8f11c616f90b52
zameer <zameeralucknowi@gmail.com>
2025-04-13 19:18:24 +0530

    first commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-pink-600'>Hi, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+          Zameer A Lucknowi
+        </h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+          I'm a Full Stack Developer.
+        </h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+        I’m a backend developer passionate about building scalable applications. Lately, I’ve been exploring the frontend with React.js.  I'm passionate about writing clean, efficient code and constantly exploring better ways to build modern web applications.
+        </p>
+        <div>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 cursor-p '>
+            View Work
+            <span className='group-hover:rotate-90 duration-300'>
+              <HiArrowNarrowRight className='ml-3 ' />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+import dreamMart from '../assets/projects/dreamMart.png'
+import webChat from '../assets/projects/webChats.png'
+import yt from '../assets/projects/yt.png'
+
+export const data=[
+    {
+        id:1,
+        name:"Dream Mart web App",
+        image:dreamMart, 
+        github:"https://github.com/zameeralucknowi/Dream_mart_apis",
+        live:"https://dreammart-webapp.onrender.com",
+    },
+    {
+        id:2,
+        name:"Web Chats App",
+        image:webChat,
+        github:"https://github.com/zameeralucknowi/web-chats-apis",
+        live:"https://web-chat-0xdb.onrender.com",
+    },
+    {
+        id:3,
+        name:"Youtube Clone",
+        image:yt,
+        github:"https://github.com/zameeralucknowi/youtube-clone",
+        live:"https://youtube-clone-giw1.onrender.com",
+    }
+
+
+]
